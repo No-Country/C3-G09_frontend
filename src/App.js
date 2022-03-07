60ee920735338511255c8d790320ceabf725b12d
unknown <osskrjuarez@gmail.com>
2022-03-07 20:55:48 -0300

    browserRouter reparado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import { BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom'
 import React, {useEffect, useState } from "react";
 import Loading from "./pages/Loading";
 import './global/styles.css'
 import Restriction from "./pages/Restriction";
 import './app.css'
 import Login from "./pages/Login";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
@@ -25,16 +25,26 @@ function App() {
     <div>
 
       <Restriction/>
+      <Loading className={ loading ? "loading" : "loading active"}/>
+        
         <div className="app">
-            <Loading className={ loading ? "loading" : "loading active"}/>
+            
             {/* <Login/> */}
 
             {/*  <Pay/>  */ }
-            <Route path='/home' element={<Home/>} />
+        <BrowserRouter>
+            <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/login' element={<Login/>} />
             <Route path="/register" element={<Register/>}/>
             <Route path='/profile' element={<Profile/>}/>
             <Route path='/userdate' element={<UserDate/>}/>
+            </Routes>
+        </BrowserRouter>
             </div>
+        
+        
+        
 
     </div>
   );
